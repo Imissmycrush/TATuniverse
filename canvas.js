aa8e4c3f78f1e39e77abf7fbb956d4595b12b4b2
Imissmycrush <nguyentrongv333@gmail.com>
2024-09-14 22:48:02 +0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,15 @@
-
+(
+  function () 
+ {
+    function playMusic() 
+    {
+        var myAudio = document.getElementById('playAudio');
+        if (myAudio.duration > 0 && !myAudio.paused) 
+        {
+            // It's playing...do your job
+        } else {
+            myAudio.play();
+            // Not playing...maybe paused, stopped, or never played.
+        }
+    }
+)
